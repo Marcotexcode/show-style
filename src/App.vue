3a5cfeae375232e860a50a6b459593b2985545c1
Marco Tessitore <marcotexcode@gmail.com>
2021-10-05 15:45:37 +0200

    add show

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
+ 
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+
+    <Header/>
+
+    <Show/>
+
+
+  
   </div>
+
 </template>
 
 
 
 <script>
 
-export default {
-  name: 'App',
-  components: {
+  import Header from '@/components/Header.vue'
+  import Show from '@/components/Show.vue'
+
+
+
+  export default {
+    name: 'App',
+    components: {
+
+      Header,
+      Show
+
+    }
   }
-}
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    @import '@/style/general';
+    @import '@/style/commons';
+
 </style>
---
+++ src/components/Show.vue
@@ -0,0 +1,116 @@
+<template>
+  
+    <div class="container">
+
+        <div class="container--img--desc">
+
+             <img src="../assets/house.jpg" alt="">
+
+            <div class="descrizioni">
+                
+                <h2>Titolo casa</h2>
+                <span>Recensioni</span>
+
+            </div>
+
+        </div>
+       
+        <div class="banner-servizi">
+
+            <h2>Servizi</h2>
+
+            <ul class="el-servizi">
+                <li><i class="fas fa-bed"></i> Letti: codice</li>
+                <li><i class="fas fa-house-user"></i> Stanze: codice</li>
+                <li><i class="fas fa-restroom"></i> Bagni: codice</li>
+                <li><i class="fas fa-align-justify"></i> ecc: codice</li>
+            </ul>
+
+        </div>
+       
+
+
+    </div>
+
+</template>
+
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+
+<style lang="scss" scoped>
+
+    .container {
+        border-radius: 20px;
+        width: 100%;
+        height: 500px;
+        margin: 90px auto;
+
+        .container--img--desc {
+            height: 500px;
+            border-radius: 20px;
+            display: flex;
+            width: 90%;
+            background-color: rgba(128, 128, 128, 0.13);
+            margin: 0 auto;
+            animation: animate 2s;
+
+            img {
+            width: 50%;
+            height: 500px;
+            border-radius: 20px;
+            position: absolute;
+
+            }
+
+            .descrizioni {
+                width: 50%;
+                margin-left: 70%;
+            }
+
+        }
+
+        .banner-servizi {
+            background-color: rgba(128, 128, 128, 0.13);
+            width: 90%;
+            margin: 0 auto;
+            border-radius: 20px;
+            padding: 30px;
+            margin-top: 30px;
+
+            .el-servizi {
+                display: flex;
+                margin: 20px;
+                list-style: none;
+
+                li {
+                    margin: 0px 20px;
+                }
+            }
+
+        }
+        
+
+    }
+    
+
+    @keyframes animate {
+
+        from {
+            width: 50%;
+        }
+
+        to {
+            width: 90%;
+        }
+
+
+    }
+
+</style>
